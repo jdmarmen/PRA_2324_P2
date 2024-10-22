59bc9c91b5d1aaa9ea0bfc6c06966d885cb33436
jdmarmen <jdmarmen@etsinf.upv.es>
2024-10-22 19:59:33 +0200

    Implemented a QuickSort and a Partition function for ordering a vector, also implemented some fixes to BusquedaBinaria function

--- DyV.h
+++ DyV.h
@@ -1,14 +1,20 @@
+#pragma once
+
 #include <vector>
 #include <stdexcept>
 #include <iostream>
+#include <utility>
 
 using namespace std;
 
 template <typename T>
-void BusquedaBinaria(T valor,vector<T>& v, int ini, int fin)
+int BusquedaBinaria(T valor,vector<T>& v, int ini, int fin)
 {
 	if(ini>fin)
-		cout<<"No se encuentra el array\n";
+	{
+		throw invalid_argument("No se encuentra el array\n");
+		return -1;
+	}
 
 	int medio=(ini+fin)/2;
 
@@ -19,4 +25,34 @@ void BusquedaBinaria(T valor,vector<T>& v, int ini, int fin)
 	else
 		return BusquedaBinaria(valor,v,medio+1,fin);
 
-}	
+}
+
+template <typename T>
+void QuickSort(vector<T>& v, int ini, int fin)
+{
+	if(ini<fin)
+	{
+		int pivot=Partition(v,ini,fin);
+		QuickSort(v,ini,pivot-1);
+		QuickSort(v,pivot+1,fin);
+	}
+
+}
+
+template <typename T>
+int Partition(vector <T>& v, int ini, int fin)
+{
+	T x=v[fin];
+	int i=ini;
+	for(int j=ini;j<=fin-1;j++)
+	{
+		if(v[j]<=x)
+		{
+			swap(v[i],v[j]);
+			i++;
+		}
+	}
+	swap(v[i],v[fin]);
+	return i;
+
+}
